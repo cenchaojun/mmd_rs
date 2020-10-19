9fd56d6ba61914d2a047b17f9c822575fbd0b9b6
floatingstarZ <alexhzy@126.com>
2020-10-19 08:37:58 +0800

    a

--- configs/DOTA_hbb/retinanet_r50_fpn_2x_dota.py
+++ configs/DOTA_hbb/retinanet_r50_fpn_2x_dota.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/retinanet_r50_fpn.py'
+    '../_base_/models/retinanet_r50_fpn.py',
     '../_base_/datasets/DOTA_train_val_hbb_rs.py',
     '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
 ]
