e71e698dc8b3b48957890a59d2a492e04ae3401c
floatingstarZ <alexhzy@126.com>
2020-10-20 09:54:25 +0800

    faster

--- EXP_CONCONFIG/faster_rcnn_hbb_tv.sh
+++ EXP_CONCONFIG/faster_rcnn_hbb_tv.sh
@@ -1,5 +1,5 @@
 cd ..
-CUDA_VISIBLE_DEVICES=4,5 python train_dota.py \
+CUDA_VISIBLE_DEVICES=8,9 python train_dota.py \
 ./DOTA_configs/DOTA_hbb/faster_rcnn_r50_fpn_2x_dota.py \
 --gpus 2 \
 --no-validate \
